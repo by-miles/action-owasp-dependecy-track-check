2f6029beb8e261ec1a68d659546473f6d8ddacbe
Chris Gray <chris@tyr.vin>
2022-08-22 12:33:02 +0000

    Add NVM install

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,7 @@ case $LANGUAGE in
         cd "$NVM_DIR"
         git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
         ) && \. "$NVM_DIR/nvm.sh"
+        nvm install
         nvm use
         npm install
         npm audit fix --force
